f72dc40b25a47af9899e2517cb022cda01a009af
Adriansema <adrianjandro1@gmail.com>
2025-03-29 15:42:33 -0500

    Agregado controlador AdminController y otras cosas mas

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.vue",
+    "./resources/css/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
